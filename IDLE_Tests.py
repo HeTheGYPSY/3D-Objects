bb587d75058ca1741bca79d973ace5e952ba31b2
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2022-10-01 19:09:39 +0300

    Final Edits

--- IDLE_Tests.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-conn = mysql.connector.connect(host ="localhost",
-                               user ="GypsyThe",
-                               password ="$rt32.,Hezron186.")
-my_cursor = conn.cursor()
-conn.close()
